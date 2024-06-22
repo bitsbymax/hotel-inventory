5c7bcdd2fd6175e319ff514196de48787284883e
Maksym Vikolash <vikolashmaxim@gmail.com>
2024-06-22 22:59:24 +0300

    updates

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, APP_INITIALIZER, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  APP_INITIALIZER,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,20 +10,20 @@ import { APP_CONFIG, APP_SERVICE_CONFIG } from './config/config.service';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { requestInterceptor } from './request.interceptor';
 import { initFactory, InitService } from './services/init.service';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     { provide: APP_SERVICE_CONFIG, useValue: APP_CONFIG },
-    provideHttpClient(
-      withInterceptors([requestInterceptor])
-    ),
+    provideHttpClient(withInterceptors([requestInterceptor])),
     {
       provide: APP_INITIALIZER,
       useFactory: initFactory,
       deps: [InitService],
       multi: true,
-    }
+    },
+    provideAnimationsAsync(),
   ],
 };
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
 
 export const routes: Routes = [];
